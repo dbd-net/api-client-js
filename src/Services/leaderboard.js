320ce62375649265ea59ddfb8ebeb8dc0e07fd74
Q <slayerment@gmail.com>
2021-08-25 21:08:10 -0600

    update leaderboard

--- src/Services/leaderboard.js
+++ src/Services/leaderboard.js
@@ -6,14 +6,30 @@ export default class Leaderboard {
     this.config = config;
   }
   
-  mostBets() {
+  list(type, period, currency) {
+    let path = '';
+    let tag = '';
+    switch (type) {
+      case 'most_bets_casino':
+        path = 'most-bets';
+        tag = 'casino';
+        break;
+      case 'top_bet_casino':
+        path = 'top-bet';
+        tag = 'casino';
+        break;
+      case 'most_bets_sports':
+        path = 'most-bets';
+        tag = 'sports';
+        break;
+      case 'top_bet_sports':
+        path = 'top-bet';
+        tag = 'sports';
+        break;
+    }
+    let endpoint ='leaderboard/reports/' + path + '?filter[tags][]=' + tag + '&filter[period]=' + period + '&filter[display-currency.display-unit]=' + currency;
     let client = new Client(this.config);
-    return client.request('GET', 'leaderboard/reports/most-bets');
-  }
-
-  topBet() {
-    let client = new Client(this.config);
-    return client.request('GET', 'leaderboard/reports/top-bet');
+    return client.request('GET', endpoint);
   }
 
 }
