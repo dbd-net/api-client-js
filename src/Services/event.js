1a87cdf9786f98afd6a909e2ddd10a60176fd60b
Q <slayerment@gmail.com>
2021-12-29 18:16:05 -0700

    token scope update

--- src/Services/event.js
+++ src/Services/event.js
@@ -3,9 +3,9 @@ import Token from '../token';
 export default class Event {
 
   constructor() {
-    let token = new Token();
+    this.token = new Token();
     this.validDomain = false;
-    if (token.getDomainId() == 1000001)  {
+    if (this.token.getDomainId() == 1000001)  {
       this.validDomain = true;
     }
   }
@@ -64,8 +64,8 @@ export default class Event {
       }
     };
     payload.data.relationships = {'authorizationToken': data};
-    token.getSafeOrigins().forEach(e => {
-      if (e == token.getWebUri()) {
+    this.token.getSafeOrigins().forEach(e => {
+      if (e == this.token.getWebUri()) {
         window.parent.postMessage(payload, e);
       }
     });
