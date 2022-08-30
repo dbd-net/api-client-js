caddcdaf30c0af4f4031708ede14b4fc2baa147c
Q <slayerment@gmail.com>
2022-08-30 17:37:52 -0600

    DAT-34 add new leaderboard endpoints

--- src/Services/leaderboard.js
+++ src/Services/leaderboard.js
@@ -6,32 +6,14 @@ export default class Leaderboard {
     this.config = config;
   }
   
-  list(type, period, currency = '') {
-    let path = '';
-    let category = '';
-    switch (type) {
-      case 'most_bets_casino':
-        path = 'most-bets';
-        category = 'casino';
-        break;
-      case 'top_bet_casino':
-        path = 'top-bet';
-        category = 'casino';
-        break;
-      case 'most_bets_sports':
-        path = 'most-bets';
-        category = 'sports';
-        break;
-      case 'top_bet_sports':
-        path = 'top-bet';
-        category = 'sports';
-        break;
-    }
-    let currency_filter = '';
-    // if (currency != '') {
-      currency_filter = '&filter[display-currency.display-unit]=' + currency;
-    // }
-    let endpoint ='leaderboard/reports/' + path + '?page[size]=50&filter[service-category]=' + category + '&filter[period]=' + period + currency_filter;
+  results(uuid) {
+    let endpoint = 'leaderboard/' + uuid + '/intervals/current';
+    let client = new Client(this.config);
+    return client.request('GET', endpoint);
+  }
+
+  hof(uuid) {
+    let endpoint = 'leaderboard/' + uuid + '/reports/hof';
     let client = new Client(this.config);
     return client.request('GET', endpoint);
   }
